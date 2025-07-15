1e4aabba7099c7b378217eb1ccfa5efaeb9c1616
lquevedo030702 <lquevedo030702@gmail.com>
2025-07-14 21:31:58 -0500

    añadiendo las app.js

---
+++ backend/app.js
@@ -0,0 +1,21 @@
+const express = require('express'); //importamos express que es el framework que usamos para crear servidores en Node.js
+const cors = require('cors'); //importamos cors, que es un middleware que permite el intercambio de recursos entre diferentes dominios (para hacer peticiones desde el frontend al backend)
+const dotenv = require('dotenv'); //importamos dotenv, que es un paquete que carga variables de entorno desde un archivo .env (donde podemos guardar configuraciones sensibles como puertos o claves API, usuarios, contraseñas, etc.)
+const app = express(); //creamos una instancia de express, que es nuestra aplicación web
+
+dotenv.config(); //activa la carga de variables de entorno desde el archivo .env
+app.use(cors()); // activamos el middleware cors para permitir peticiones desde otros dominios (como el frontend 4444)
+app.use(express.json()); //activamos el middleware express.json() para poder recibir datos en formato JSON en las peticiones HTTP(como POST o PUT)
+
+app.post('/api/productos', (req, res) => {
+    res.json([
+        { id: 1, name: 'Monstera', price: 41000 },
+        { id: 2, name: 'Suculenta', price: 20000 },
+        { id: 3, name: 'Sanseviera', price: 30000 }
+    ]);
+}); // definimos una ruta POST que recibe datos de productos y responde con un JSON de ejemplo
+
+const PORT = process.env.PORT || 3333;// definimos el puerto en el que escuchará nuestro servidor, ya sea el que esté en las variables de entorno o el 3333 por defecto
+app.listen(PORT, () => {
+    console.log(`Backend corriendo en http://localhost:${PORT}`);
+});// iniciamos el servidor y mostramos un mensaje en la consola indicando que está corriendo
---
+++ frontend/app.js
@@ -0,0 +1,16 @@
+const express = require('express'); //importamos express que es el framework que usamos para crear servidores en Node.js
+const path = require('path'); //importamos path, que es un módulo de Node.js para manejar rutas de archivos
+const app = express(); //creamos una instancia de express, que es nuestra aplicación web
+
+app.set('view engine', 'ejs'); // configuramos EJS como el motor de plantillas para renderizar vistas
+app.set('views', path.join(__dirname, 'views')); // configuramos la carpeta donde están las vistas EJS
+app.use(express.static(path.join(__dirname, 'public'))); // configuramos la carpeta pública para servir archivos estáticos como CSS, JS e imágenes
+
+app.get('/', (req, res) => {
+    res.render('paginas/inicio'); // renderizamos la vista de inicio
+});
+
+const PORT = 4444; // definimos el puerto en el que escuchará nuestro servidor, que es el 4444 para el frontend
+app.listen(PORT, () => {
+    console.log(`Frontend corriendo en http://localhost:${PORT}`); // iniciamos el servidor y mostramos un mensaje en la consola indicando que está corriendo
+});
